3fecddd015a2db43ec2980da5c4238de0896e45e
harrybands <90954820+harrybands@users.noreply.github.com>
2022-02-20 00:09:37 -0700

    Update schema.sql

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS class_sections;
 DROP TABLE IF EXISTS instructors;
 DROP TABLE IF EXISTS academic_titles;
 DROP TABLE IF EXISTS students;
-DROP TABLE IF EXISTS classes
+DROP TABLE IF EXISTS classes;
 DROP FUNCTION IF EXISTS convert_to_grade_point;
 CREATE TABLE IF NOT EXISTS classes(
  class_id INT AUTO_INCREMENT,
@@ -40,5 +40,53 @@ create table terms(
 term_id int auto_increment not null, 
 name varchar(80) not null,
 primary key(term_id)
-)
+);
 
+create table class_sections(
+class_section_id int auto_increment,
+class_id int not null,
+instructor_id int not null, 
+term_id int not null, 
+primary key(class_section_id),
+foreign key(class_id) references classes(class_id), 
+foreign key(instructor_id) references instructors(instructor_id), 
+foreign key(term_id) references terms(term_id)
+);
+
+create table grades(
+grade_id int auto_increment not null,
+letter_grade char(2) not null, 
+primary key(grade_id)
+);
+
+create table class_registrations(
+class_section_id int not null,
+class_registration_id int auto_increment not null,
+student_id int, 
+grade_id int, 
+signup_timestamp datetime default current_timestamp,
+foreign key(class_section_id) references class_sections(class_section_id), 
+primary key(class_registration_id)
+);
+
+DELIMITER $$
+CREATE FUNCTION convert_to_grade_point(letter_grade char(2))
+ RETURNS char
+ DETERMINISTIC
+BEGIN
+ declare grade_point int; 
+ if letter_grade = 'A' then
+	set grade_point = 4; 
+elseif letter_grade = 'B' then 
+	set grade_point = 3; 
+elseif letter_grade = 'C' then 
+	set grade_point = 2; 
+elseif letter_grade = 'D' then 
+	set grade_point = 1; 
+elseif letter_grade = 'F' then 
+	set grade_point = 0;
+else
+	set grade_point = null; 
+end if;
+return grade_point; 
+END $$
